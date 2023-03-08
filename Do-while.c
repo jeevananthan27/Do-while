aaf5689c505d2dde07f3fb90bdba09e50fcfdc8f
JEEVANANTHAN A <106263059+jeevananthan27@users.noreply.github.com>
2023-03-08 20:42:09 +0530

    Add files via upload

---
+++ Do-while.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+void main()
+
+{
+    
+
+//Do while loops
+
+    int i=1,a;
+
+    printf("%d",a);
+
+    scanf("%d",&a);
+
+    do
+
+    {
+
+        printf("%d*%d=%d\n",a,i,a*i);
+
+        i=i+1;
+        
+    }
+
+    while(i<=10);
+
+}
